53c19cc9b22fdf4eab7746f16578731a1b9e0cf8
zhujikun <zhujikun@youzan.com>
2022-03-25 15:07:46 +0800

    二分查找

--- leetcode/src/main/java/com/leet/array/排序数组中查找元素的第一个和最后一个位置.java
+++ leetcode/src/main/java/com/leet/array/排序数组中查找元素的第一个和最后一个位置.java
@@ -8,17 +8,17 @@ package com.leet.array;
  */
 public class 排序数组中查找元素的第一个和最后一个位置 {
 
-    private static int[] arr = {-1,0,3,5,9,12};
+    private static int[] arr = {-1,0,3,5,5, 5, 5, 5, 9,12};
 
-    private static int target = 4;
+    private static int target = 5;
 
 
     /**
      * 左、右分开计算
      */
     public static void main(String[] args) {
-
-
+        System.out.println(leftPoint(arr, 5));
+        System.out.println(rightPoint(arr, 5));
 
     }
 
@@ -30,12 +30,67 @@ public class 排序数组中查找元素的第一个和最后一个位置 {
         int left = 0;
         int right = arr.length - 1;
 
+        int point = -1;
+
         while (left <= right) {
+            // 校验最右侧的数据是否小于目标值, 如果小于，说明target不在这个区间
+            if (arr[right] < target) {
+                break;
+            }
 
+            int index = (left + right) / 2;
+            if (arr[index] == target) {
+                point = index;
+            }
 
-
+            // 区间缩小, 向左
+            // 主要要包含等于，等于的时候就继续向左找，知道找到最左边的数据
+            if (arr[index] >= target) {
+                right = index - 1;
+            }
+            // 向右
+            else {
+                left = index + 1;
+            }
         }
-        return -1;
+        return point;
     }
 
+
+    /**
+     * 最右
+     */
+    private static int rightPoint(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        int point = -1;
+
+        while (left <= right) {
+            // 校验最左侧的数据是否大于目标值, 如果大于，说明target不在这个区间，， 处于更左边的区间
+            if (arr[left] > target) {
+                break;
+            }
+
+            int index = (left + right) / 2;
+            if (arr[index] == target) {
+                point = index;
+            }
+
+            // 区间缩小,
+            // 向右查找
+            // 主要要包含等于，等于的时候就继续向右找，知道找到最右边的数据
+            if (arr[index] <= target) {
+                left = index + 1;
+            }
+            // 向左
+            else {
+                right = index - 1;
+            }
+        }
+        return point;
+    }
+
+
 }
+
